28bc3b51b102ba9c51e7749c5320a119a171df37
Erika <erika.paige.handley@gmail.com>
2020-09-24 17:02:39 -0700

    DAy 28: Local Storage

--- js/localStorageDisplay.js
+++ js/localStorageDisplay.js
@@ -1,45 +1,5 @@
-// ==== FUNCTIONS FOR SET INTERVAL ==== //
-let hi = setInterval(() => {
-  console.log('hi')
-}, 500)
-document.getElementById('hiTicker').addEventListener('click', () => {
-  clearTimeout(hi)
-})
-// random number generator 0 - 100
-let randomNumberGen = setInterval(() => {
-  console.log(Math.floor(Math.random() * 100))
-}, 500)
-document.getElementById('stopGenerator').addEventListener('click', () => {
-  clearTimeout(randomNumberGen)
-})
-
-
-// ==== FUNCTIONS FOR TIME OUTS ==== //
-console.log('10 seconds left')
-
-let fiveSeconds = setTimeout(() => {
-  console.log('5 seconds left')
-}, 5000)
-
-// abort a setTimeOut by clearing the timeout so give it a name
-// clearTimeout(fiveSeconds)
-
-let eightSeconds = setTimeout(() => {
-  console.log('2 seconds left')
-}, 8000)
-
-let timesUp = setTimeout(() => {
-  console.log('time is up!')
-}, 10000)
-
-// button responsible for stopping timed event
-document.getElementById('abort').addEventListener('click', () => {
-  clearTimeout(timesUp)
-})
-
-
-
 // ==== FUNCTIONS FOR LOCAL STORAGE ==== //
+
 // make button clickable
 document.getElementById('signUp').addEventListener('click', (event) => {
   event.preventDefault()
@@ -53,6 +13,7 @@ document.getElementById('signUp').addEventListener('click', (event) => {
   // stringify turns object into string so it can be stored in string
   localStorage.setItem('user', JSON.stringify(user))
 })
+
 // to display user
 if (localStorage.getItem('user')) {
   // create new object to get information from local storage to display on inner HTML
