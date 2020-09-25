90dbe119eb87b312e67d6acf7ba1f2f67b4e26af
Erika <erika.paige.handley@gmail.com>
2020-09-24 17:18:21 -0700

    Day 28: Timed Events in JS

---
+++ js/jsDay5TimedEvents.js
@@ -0,0 +1,38 @@
+// ==== FUNCTIONS FOR SET INTERVAL ==== //
+let hi = setInterval(() => {
+  console.log('hi')
+}, 500)
+document.getElementById('hiTicker').addEventListener('click', () => {
+  clearTimeout(hi)
+})
+// random number generator 0 - 100
+let randomNumberGen = setInterval(() => {
+  console.log(Math.floor(Math.random() * 100))
+}, 500)
+document.getElementById('stopGenerator').addEventListener('click', () => {
+  clearTimeout(randomNumberGen)
+})
+
+
+// ==== FUNCTIONS FOR TIME OUTS ==== //
+console.log('10 seconds left')
+
+let fiveSeconds = setTimeout(() => {
+  console.log('5 seconds left')
+}, 5000)
+
+// abort a setTimeOut by clearing the timeout so give it a name
+// clearTimeout(fiveSeconds)
+
+let eightSeconds = setTimeout(() => {
+  console.log('2 seconds left')
+}, 8000)
+
+let timesUp = setTimeout(() => {
+  console.log('time is up!')
+}, 10000)
+
+// button responsible for stopping timed event
+document.getElementById('abort').addEventListener('click', () => {
+  clearTimeout(timesUp)
+})
